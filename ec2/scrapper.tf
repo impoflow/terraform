68b34d82ac9d93dc5dda9a39dfc14e64b6e1e7d1
Ricardo <tu-correo@ejemplo.com>
2025-01-17 11:24:56 +0000

    Scrapper Work Load

--- ec2/scrapper.tf
+++ ec2/scrapper.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "scrapper_sg" {
 
 resource "aws_instance" "scrapper_instance" {
   ami                    = "ami-0fff1b9a61dec8a5f" # Amazon Linux 2 AMI
-  instance_type          = "t2.micro"
+  instance_type          = "t2.medium"
   subnet_id              = var.subnet-id
   key_name               = aws_key_pair.ssh_key.key_name
   vpc_security_group_ids = [aws_security_group.scrapper_sg.id]
@@ -57,9 +57,21 @@ resource "aws_instance" "scrapper_instance" {
 
               # Run the container
               cat <<EOT >> /home/ec2-user/run.sh
-              docker run -d -e "BUCKET_NAME=${var.bucket-name}" -e "REGION=us-east-1" -e "USERS=300" -e "URL=https://github.com/search?q=java+agenda&type=repositories&p=" ${var.docker-username}/github-scrapper
-              docker run -d -e "BUCKET_NAME=${var.bucket-name}" -e "REGION=us-east-1" -e "USERS=300" -e "URL=https://github.com/search?q=todo+list+java&type=repositories&p=" ${var.docker-username}/github-scrapper
+              docker run -d -name scrapper -e "BUCKET_NAME=${var.bucket-name}" -e "REGION=us-east-1" -e "USERS=300" -e "URL=https://github.com/search?q=java+agenda&type=repositories&p=" ${var.docker-username}/github-scrapper
+              sleep 120
+              docker stop scrapper
+              docker container prune
+
+              docker run -d -name scrapper -e "BUCKET_NAME=${var.bucket-name}" -e "REGION=us-east-1" -e "USERS=300" -e "URL=https://github.com/search?q=todo+list+java&type=repositories&p=" ${var.docker-username}/github-scrapper
+              sleep 120
+              docker stop scrapper
+              docker container prune
+
+
               docker run -d -e "BUCKET_NAME=${var.bucket-name}" -e "REGION=us-east-1" -e "USERS=300" -e "URL=https://github.com/search?q=calculator+java&type=repositories&p=" ${var.docker-username}/github-scrapper
+              sleep 120
+              docker stop scrapper
+              docker container prune
               EOT
 
               chmod +x /home/ec2-user/run.sh
